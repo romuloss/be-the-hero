fd9ff8cda55840177f9045504747e5124a2a2187
romuloss <romuloss@gmail.com>
2020-03-25 19:04:28 -0300

    Frontend do app

--- README.md
+++
@@ -1,2 +0,0 @@
-# be-the-hero
-# be-the-hero
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css'
+
+import Routes from './routes';
+
 
 function App() {
+/* Exemplo de Estado
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>Contador {counter} Semana OnmiStack - Be The Hero </Header>
+      <button onClick={increment}>Incrementar</button>
     </div>
+  );*/
+
+  return (
+          <Routes />
+    
   );
+
 }
 
 export default App;
